dd62bd396b70425e505ea617d798b788ad4583d9
Rabia-Rajput <rajput.rabia@outlook.com>
2024-03-28 03:24:11 +0100

    write code for different button in button.js file

--- src/Button.js
+++ src/Button.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 function Button({
     children,
     primary,
@@ -11,7 +13,26 @@ function Button({
     // if ( primary && secondary){
     //     throw new Error('only one of primary and secondary shloud be provided')
     // }
-    return <button>{children}</button>;
+    return <button className='bg-red-500'>{children}</button>;
 }
+Button.propTypes ={
+    checkVariationValue: ({primary,secondary,
+        success,
+        warning,
+        danger})=>{
+
+            const count = Number(!!primary)+
+            Number(!!secondary)+
+            Number(!!warning)+
+            Number(!!success)+
+            Number(!!danger)
+            if (count>1)
+     {
+        return new Error ('only one can be true');
+    }
+    },
+    
+
+};
 
 export default Button;
