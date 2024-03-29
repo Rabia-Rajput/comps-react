6762d92e22b4b4b7f3a0586da1d9e9738465ca65
Rabia-Rajput <rajput.rabia@outlook.com>
2024-03-30 00:36:46 +0100

    add some button styling code in App.js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Button from "./Button";
 import {GoBell, GoCloudDownload, GoDatabase, GoCheck, GoClippy} from 'react-icons/go';
 function App (){
+    const handleClick=()=>{
+        console.log('clicked');
+
+    };
     return <div>
-        <div><Button success rounded outline><GoCloudDownload/>Click me </Button></div>
+        <div><Button success rounded outline className='mb-5' onClick={handleClick}><GoCloudDownload/>Click me </Button></div>
         <div><Button danger outline> <GoBell/>Buy now!</Button></div>
         <div><Button warning><GoClippy/>See Data</Button></div>
         <div><Button secondary outline> <GoDatabase/> Hide Ads</Button></div>
