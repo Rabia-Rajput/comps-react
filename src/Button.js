c5bc11064f4cd58010ad473a2236b40a9b5d07d0
Rabia-Rajput <rajput.rabia@outlook.com>
2024-03-29 22:27:13 +0100

    add more styling in Button.js file

--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,5 @@
-import PropTypes from 'prop-types';
+import className from "classnames";
+
 
 function Button({
     children,
@@ -13,7 +14,15 @@ function Button({
     // if ( primary && secondary){
     //     throw new Error('only one of primary and secondary shloud be provided')
     // }
-    return <button className='px-3 py-1.5 border border-blue-600 bg-blue-500 text-white'>{children}</button>;
+const classes = className('px-3 py-1.5 border',{
+    'border-blue-500 bg-blue-500 text-white':primary,
+    'border-gray-900 bg-gray-900 text-white':secondary,
+    'border-green-500 bg-green-500 text-white':success,
+    'border-yellow-400 bg-yellow-400 text-white':warning,
+    'border-red-500 bg-red-500 text-white':danger,
+});
+
+    return <button className={classes}>{children}</button>;
 }
 Button.propTypes ={
     checkVariationValue: ({primary,secondary,
