27177619c31ff091469a449ccfecbc450c5323de
Rabia-Rajput <rajput.rabia@outlook.com>
2024-03-29 23:18:49 +0100

    code for icons in App.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Button from "./Button";
-
+import {GoBell, GoCloudDownload, GoDatabase, GoCheck, GoClippy} from 'react-icons/go';
 function App (){
     return <div>
-        <div><Button success rounded outline>Click me </Button></div>
-        <div><Button danger outline>Buy now!</Button></div>
-        <div><Button warning>See Data</Button></div>
-        <div><Button secondary outline>Hide Ads</Button></div>
-        <div><Button primary rounded>Something!</Button></div>
+        <div><Button success rounded outline><GoCloudDownload/>Click me </Button></div>
+        <div><Button danger outline> <GoBell/>Buy now!</Button></div>
+        <div><Button warning><GoClippy/>See Data</Button></div>
+        <div><Button secondary outline> <GoDatabase/> Hide Ads</Button></div>
+        <div><Button primary rounded><GoCheck/>Something!</Button></div>
        
 
     </div>;
