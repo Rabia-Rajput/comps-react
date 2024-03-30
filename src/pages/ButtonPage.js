3bd3eb23c08054541c69f28f578b2461369f96f7
Rabia-Rajput <rajput.rabia@outlook.com>
2024-03-31 00:20:38 +0100

    ButtonPage.js file created

---
+++ src/pages/ButtonPage.js
@@ -0,0 +1,21 @@
+import Button from "../components/Button";
+import {GoBell, GoCloudDownload, GoDatabase, GoCheck, GoClippy} from 'react-icons/go';
+function ButtonPage (){
+    const handleClick=()=>{
+        console.log('clicked');
+
+    };
+    return <div>
+        <div><Button success rounded outline className='mb-5' onClick={handleClick}><GoCloudDownload/>Click me </Button></div>
+        <div><Button danger outline> <GoBell/>Buy now!</Button></div>
+        <div><Button warning><GoClippy/>See Data</Button></div>
+        <div><Button secondary outline> <GoDatabase/> Hide Ads</Button></div>
+        <div><Button primary rounded><GoCheck/>Something!</Button></div>
+       
+
+    </div>;
+}
+
+
+
+export default ButtonPage;
