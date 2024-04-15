d6eafbec49d1798b5e5dd444387d690f3024010c
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-15 21:49:35 +0200

    upadted Dropdown.js file

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import {useState} from 'react'
 
 const Dropdown = ({options} ) => {
+    const [isOpen, setIsOpen]= useState(false);
+    const handleClick=()=>{
+        setIsOpen(!isOpen);
+      // OR //  isOpen( (currentIsOpen) => !isOpen);
+    };
     const renderedOptions=options.map((option)=>{
-       return <div>
+       return <div key={option.value}>
         {option.label}
        </div>
 
     });
   return (
-    <div>Dropdown</div>
-  )
+    <div>
+        <div onClick={handleClick}>Select...</div>
+       { isOpen && <div>{renderedOptions} </div>}
+    </div>
+  );
 }
 
 export default Dropdown
