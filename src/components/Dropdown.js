f9ce30c2cb28e5b0dad6a92a0c11f497f4aa3df1
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-15 21:36:28 +0200

    upadted Dropdown.js file

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 const Dropdown = ({options} ) => {
+    const renderedOptions=options.map((option)=>{
+       return <div>
+        {option.label}
+       </div>
+
+    });
   return (
     <div>Dropdown</div>
   )
