28f853a8d5ab0bc7298a08d6c002ae0e4cfee068
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-15 18:25:41 +0200

    upadted App.js file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import Accordion from "./components/Accordion";
-function App (){
-const items =[
-    { id: 'hdjvjnv',
-        label: 'can I use React on a Project?',
-      content:' You can use React on any project You can use React on any project You can use React on any project You can use React on any project You can use React on any project You can use React on any project'
-      },
+function App() {
+  const items = [
+    {
+      id: "hdjvjnv",
+      label: "can I use React on a Project?",
+      content:
+        " You can use React on any project You can use React on any project You can use React on any project You can use React on any project You can use React on any project You can use React on any project",
+    },
 
     {
-        id:'ghdiig',
-        label: 'can I use js on a Project?',
-    content:' You can use js on any project You can use React on any project You can use React on any project You can use React on any project You can use React on any project You can use React on any project You can use React on any project'
-},
+      id: "ghdiig",
+      label: "can I use js on a Project?",
+      content:
+        " You can use js on any project You can use React on any project You can use React on any project You can use React on any project You can use React on any project You can use React on any project You can use React on any project",
+    },
 
     {
-        id:'loasopid',
-        
-        label: 'can I use CSS on a Project?',
-    content:' You can use CSS on any projectYou can use React on any project You can use React on any project You can use React on any project You can use React on any project You can use React on any project You can use React on any project'
+      id: "loasopid",
+
+      label: "can I use CSS on a Project?",
+      content:
+        " You can use CSS on any projectYou can use React on any project You can use React on any project You can use React on any project You can use React on any project You can use React on any project You can use React on any project",
+    },
+  ];
+
+  return <Accordion items={items} />;
 }
-]
-
-    return <Accordion items={items}/>;
-}
-
-
 
 export default App;
