b321b7d69206532d257f912e1438919ba68e70a0
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-15 20:53:57 +0200

    upadted App.js file

--- src/App.js
+++ src/App.js
@@ -1,30 +1,6 @@
-import Accordion from "./components/Accordion";
+
 function App() {
-  const items = [
-    {
-      id: "hdjvjnv",
-      label: "can I use React on a Project?",
-      content:
-        " You can use React on any project You can use React on any project You can use React on any project You can use React on any project You can use React on any project You can use React on any project",
-    },
 
-    {
-      id: "ghdiig",
-      label: "can I use js on a Project?",
-      content:
-        " You can use js on any project You can use React on any project You can use React on any project You can use React on any project You can use React on any project You can use React on any project You can use React on any project",
-    },
-
-    {
-      id: "loasopid",
-
-      label: "can I use CSS on a Project?",
-      content:
-        " You can use CSS on any projectYou can use React on any project You can use React on any project You can use React on any project You can use React on any project You can use React on any project You can use React on any project",
-    },
-  ];
-
-  return <Accordion items={items} />;
 }
 
 export default App;
