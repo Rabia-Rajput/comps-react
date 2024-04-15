6db0f2bb26b8f60e61e09cbefcc18bd9e8823e7f
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-15 15:56:46 +0200

    updated Accordion.js file

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { GoChevronDown , GoChevronLeft } from "react-icons/go";
+
 
 function Accordion ({items}){
     const [expandedIndex, setExpandedIndex ] = useState(0);
@@ -8,11 +10,14 @@ function Accordion ({items}){
 
     const renderedItems = items.map((item, index)=>{
        const isExpanded = index === expandedIndex;
+       const icon = <span> {isExpanded ? <GoChevronDown /> : <GoChevronLeft />} </span>;
       
        
         return(
             <div key={item.id}>
-                <div onClick={()=> handleClick(index)}>{item.label}</div>
+                <div onClick={()=> handleClick(index)}>
+                    {icon}
+                    {item.label}</div>
                 {isExpanded && <div>{item.content}</div>}
                 
             </div>
