bb71e41ac7fe5d300fae22ecf6ea84451f620a4f
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-15 17:51:59 +0200

    updated Accordion.js file

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,27 +3,42 @@ import { GoChevronDown , GoChevronLeft } from "react-icons/go";
 
 
 function Accordion ({items}){
-    const [expandedIndex, setExpandedIndex ] = useState(0);
+    const [expandedIndex, setExpandedIndex ] = useState(-1);
     const handleClick=(nextIndex)=>{
-        setExpandedIndex(nextIndex);
+        setExpandedIndex((currentExpandedIndex)=>{
+            if(currentExpandedIndex===nextIndex){
+                return -1;
+            }
+            else{
+                return nextIndex;
+            }
+        });
+        // if(expandedIndex===nextIndex){
+        //     setExpandedIndex(-1);
+        // }
+        // else{
+        //     setExpandedIndex(nextIndex);
+        // }
        };
 
     const renderedItems = items.map((item, index)=>{
        const isExpanded = index === expandedIndex;
-       const icon = <span> {isExpanded ? <GoChevronDown /> : <GoChevronLeft />} </span>;
+       const icon = <span className="text-2xl" > {isExpanded ? <GoChevronDown /> : <GoChevronLeft />} </span>;
       
        
         return(
             <div key={item.id}>
-                <div onClick={()=> handleClick(index)}>
+                <div className="flex  justify-between p-3 bg-gray-50 border-b items-center cursor-pointer" onClick={()=> handleClick(index)}>
+                    
+                    {item.label}
                     {icon}
-                    {item.label}</div>
-                {isExpanded && <div>{item.content}</div>}
+                    </div>
+                {isExpanded && <div className="border-b p-5" >{item.content}</div>}
                 
             </div>
         )
     });
-    return <div> {renderedItems} </div>;
+    return <div  className="border-x border-t rounded" > {renderedItems} </div>;
 }
 
 export default Accordion;
