98c627c4ef6c9d0e38cd3079c5e96914717ebfd7
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-16 14:39:30 +0200

    upadted App.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import {useState} from 'react'
 import Dropdown from "./components/Dropdown";
 
 function App() {
+    const [selection, setSelection]= useState(null);
+    const handleSelect=(option)=>{
+        setSelection(Option);
+    };
     const options = [
         {label:'Red' , value:'red'},
         {label:'Green' , value:'green'},
         {label:'Blue' , value:'blue'},
        
     ];
-    return<Dropdown options={options}/>
+    return<Dropdown options={options} selection={selection} onSelect={handleSelect} />
 
 }
 
