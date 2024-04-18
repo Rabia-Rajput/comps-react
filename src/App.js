cc58caecee0d557c56bf0231058d2b98358e578d
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-18 17:36:29 +0200

    upadted App.js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import Link from "./components/Link";
+import Route from "./components/Route";
+import AccordionPage from './pages/AccordionPage';
+import DropdownPage from './pages/DropdownPage';
+function App  () {
+  return   <div>
+        <Link to='/accordion'> Go to accordion </Link>
+        <Link to='/dropdown'> Go to dropdown</Link>
+        <div>
+          <Route path='/accordion'>
+         <AccordionPage/>   
+            
+             </Route>
+             <Route path='/dropdown'>
+         <DropdownPage/>   
+            
+             </Route>
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
+        </div>
+    </div>
+ 
 }
 
 export default App;
