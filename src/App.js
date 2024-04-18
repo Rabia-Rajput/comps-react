4c42bd2d817ded9cc0574843ef47d140b3ee1bf9
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-18 14:07:47 +0200

    upadted App.js file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
-import {useState} from 'react'
-import Dropdown from "./components/Dropdown";
 
-function App() {
-    const [selection, setSelection]= useState(null);
-    const handleSelect=(option)=>{
-        setSelection(option);
-    };
-    const options = [
-        {label:'Red' , value:'red'},
-        {label:'Green' , value:'green'},
-        {label:'Blue' , value:'blue'},
-       
-    ];
-    return<Dropdown options={options} selection={selection} onSelect={handleSelect} />
-
+const App = () => {
+  return (
+    <div>App</div>
+  )
 }
 
 export default App;
