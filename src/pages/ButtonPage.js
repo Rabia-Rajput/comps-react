09b64b732f29556a1fdffcce6c9d906476b2574a
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-19 17:43:47 +0200

    ButtonPage.js file updated

--- src/pages/ButtonPage.js
+++ src/pages/ButtonPage.js
@@ -9,8 +9,8 @@ function ButtonPage (){
         <div><Button success rounded outline className='mb-5' onClick={handleClick}><GoCloudDownload/>Click me </Button></div>
         <div><Button danger outline> <GoBell/>Buy now!</Button></div>
         <div><Button warning><GoClippy/>See Data</Button></div>
-        <div><Button secondary outline> <GoDatabase/> Hide Ads</Button></div>
-        <div><Button primary rounded><GoCheck/>Something!</Button></div>
+        <div><Button primary outline> <GoDatabase/> Hide Ads</Button></div>
+        <div><Button secondary  rounded><GoCheck/>Something!</Button></div>
        
 
     </div>;
