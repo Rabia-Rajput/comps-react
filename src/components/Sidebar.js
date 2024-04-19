da12171a7d6254f260b30b22e0e1f39cea24fa5c
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-19 15:26:49 +0200

    Sidebar.js file updated

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -15,7 +15,7 @@ const Sidebar = () => {
 
     });
   return (
-    <div className="sticky top-0 overflow-y-scroll flex flex-col">
+    <div className="sticky top-0  flex flex-col">
     {renderedLinks}
     </div>
   )
