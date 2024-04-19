2a52de9834a7267e228463f34c2a0f9d00c1de4d
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-19 17:42:44 +0200

    upadted Button.js file

--- src/components/Button.js
+++ src/components/Button.js
@@ -18,7 +18,7 @@ function Button({
     const classes = twMerge(
         className(rest.className,' flex items-center  px-3 py-1.5 border my-1.5 ml-3', {
           'border-blue-500 bg-blue-500 text-white': primary,
-          'border-gray-900 bg-gray-900 text-white': secondary,
+          'border-gray-900 bg-blue-600 text-white': secondary,
           'border-green-500 bg-green-500 text-white': success,
           'border-yellow-400 bg-yellow-400 text-white': warning,
           'border-red-500 bg-red-500 text-white': danger,
