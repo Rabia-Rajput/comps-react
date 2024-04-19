bda16c46b95b5c08057cf4f3308c90d351d6a860
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-19 15:27:09 +0200

    upadted App.js file

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import AccordionPage from './pages/AccordionPage';
 import DropdownPage from './pages/DropdownPage';
 import ButtonPage from './pages/ButtonPage';
 function App  () {
-  return   <div>
+  return   <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
       <Sidebar/>
-        <div>
+        <div className="col-span-5">
           <Route path='/accordion'>
          <AccordionPage/>   
             
