2685d9cb5feda024943b644fdc5130c36f0cf505
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-19 15:17:13 +0200

    upadted App.js file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Route from "./components/Route";
 import Sidebar from "./components/Sidebar";
 import AccordionPage from './pages/AccordionPage';
 import DropdownPage from './pages/DropdownPage';
+import ButtonPage from './pages/ButtonPage';
 function App  () {
   return   <div>
       <Sidebar/>
@@ -14,6 +15,10 @@ function App  () {
              <Route path='/'>
          <DropdownPage/>   
             
+             </Route>
+             <Route path='/buttons'> 
+             <ButtonPage/>
+
              </Route>
 
         </div>
