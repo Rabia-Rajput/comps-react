5c668d0d26bd0d6a657de0c0d448f6bfb0fd0141
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-20 21:10:24 +0200

    upadted App.js file

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AccordionPage from './pages/AccordionPage';
 import DropdownPage from './pages/DropdownPage';
 import ButtonPage from './pages/ButtonPage';
 import ModalPage from "./pages/ModalPage";
+import TablePage from "./pages/TablePage";
 function App  () {
   return   <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
       <Sidebar/>
@@ -24,6 +25,9 @@ function App  () {
              <Route path='/modal'>
                 <ModalPage/>
 
+             </Route>
+             <Route path='/table'>
+                <TablePage/>
              </Route>
 
         </div>
