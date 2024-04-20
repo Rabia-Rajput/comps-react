9db254fbb6190a54ba099e55fb0b5a70f8f3db1a
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-20 21:33:01 +0200

    Table.js file updated

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,38 @@
 
 
-const Table = () => {
+const Table = ({data}) => {
   return (
-    <div>Table</div>
+    <table>
+      <thead>
+        <tr>
+           <th>Fruit</th> 
+           <th>color</th> 
+           <th>Score</th> 
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+            <td>Apple</td>
+            <td>color-box</td>
+            <td>3</td>
+        </tr>
+        <tr>
+            <td>Orange</td>
+            <td>color-box</td>
+            <td>5</td>
+        </tr>
+        <tr>
+            <td>Banana</td>
+            <td>color-box</td>
+            <td>1</td>
+        </tr>
+        <tr>
+            <td>Lime</td>
+            <td>color-box</td>
+            <td>4</td>
+        </tr>
+      </tbody>
+        </table>
   )
 }
 
