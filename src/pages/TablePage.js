6c43530dc6961fa8b7474a48e2e85d93410aad9f
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-20 23:14:13 +0200

    TablePage.js file updated.

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -8,9 +8,11 @@ const TablePage = () => {
     { name: "Lime", color: "bg-green-500", score: "4" },
   ];
   const config=[
-    {label:'Name'},
-    {label:'Color'},
-    {label:'Score'},
+    {label:'Name', render:(fruit)=> fruit.name,},
+    {label:'Color',render:(fruit)=> 
+  <div className=  {`p-3 m-2 ${fruit.color}`}></div>
+  },
+    {label:'Score',render:(fruit)=>fruit.score,},
 
   ];
 
