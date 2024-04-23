3d8b951c10413440f1bb6d74cf5ec13149b77a4e
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-23 21:42:08 +0200

    Table.js file updated

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,18 +3,16 @@ import { Fragment } from "react";
 const Table = ({ data, config, keyFn }) => {
   const renderedHeaders = config.map((column) => {
     if (column.header) {
-      return <Fragment key={column.label}>{column.header()} </Fragment>;
+      return <Fragment key={column.label}>{column.header()}</Fragment>;
     }
-    return <th key={column.label}> {column.label} </th>;
+    return <th key={column.label}>{column.label}</th>;
   });
   const renderedRows = data.map((rowData) => {
-    const renderedCells = config.map((column) => {
-      return (
-        <td className="p-2" key={column.label}>
-          {column.render(rowData)}{" "}
-        </td>
-      );
-    });
+    const renderedCells = config.map((column) => (
+      <td className="p-2" key={column.label}>
+        {column.render(rowData)}
+      </td>
+    ));
     return (
       <tr className="border-b" key={keyFn(rowData)}>
         {renderedCells}
