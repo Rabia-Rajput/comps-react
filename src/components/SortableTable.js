68b581ae15e0f0c7cf9f2c11822b0c9228b95550
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-23 19:06:23 +0200

    SortableTable.js file updated.

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,8 +1,26 @@
+import { useState } from "react";
 import Table from "./Table"
 
 const SortableTable = (props) => {
+    
+    const {config}=props;
+    const handleClick=(label)=>{
+       
+
+    }
+
+    const updatedConfig = config.map((column)=>{
+        if(!column.sortValue){
+            return column;
+        }
+        return {
+            ...column,
+            header: ()=> <th onClick={()=> handleClick(column.label)}> {column.label} </th>
+        }
+
+    });
   return (
-  <Table {...props} />
+  <Table {...props} config={updatedConfig} />
   );
 }
 
