684656210a640206cf1b026f562b35d02b7252ba
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-23 19:41:02 +0200

    SortableTable.js file updated.

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -2,12 +2,26 @@ import { useState } from "react";
 import Table from "./Table"
 
 const SortableTable = (props) => {
-    
+    const [sortOrder, setSortOrder]=useState(null);
+    const [sortBy, setSortBy]=useState(null);
+
     const {config}=props;
     const handleClick=(label)=>{
+if (sortOrder===null){
+    setSortOrder('asc');
+    setSortBy(label);
+}
+else if(sortOrder==='asc')
+{setSortOrder('desc');
+setSortBy(label);
+}
+else if(sortOrder==='desc'){
+    setSortOrder(null);
+    setSortBy(null);
+}
        
 
-    }
+    };
 
     const updatedConfig = config.map((column)=>{
         if(!column.sortValue){
