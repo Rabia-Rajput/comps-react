be0379457a86c514080e6afd2529d4864e6ed39f
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-24 15:29:07 +0200

    TablePage.js file updated.

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -8,24 +8,29 @@ const TablePage = () => {
     { name: "Banana", color: "bg-yellow-500", score: "1" },
     { name: "Lime", color: "bg-green-500", score: "4" },
   ];
-  const config=[
-    {label:'Name', render:(fruit)=> fruit.name,
-    sortValue:(fruit)=> fruit.name,},
-    {label:'Color',render:(fruit)=> 
-  <div className=  {`p-3 m-2 ${fruit.color}`}></div>
-  },
-    {label:'Score',render:(fruit)=>fruit.score,
-    sortValue:(fruit)=>fruit.score,
-   
-  
-  
-
-   },
-
+  const config = [
+    {
+      label: "Name",
+      render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name,
+    },
+    {
+      label: "Color",
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
+    },
+    {
+      label: "Score",
+      render: (fruit) => fruit.score,
+      sortValue: (fruit) => fruit.score,
+    },
+    {
+      label: "Squared-Score",
+      render: (fruit) => fruit.score ** 2,
+      sortValue: (fruit) => fruit.score,
+    },
   ];
-  const keyFn = (fruit)=>{
+  const keyFn = (fruit) => {
     return fruit.name;
-
   };
 
   return (
