532172014831180043b3531c42f39bae7f86f10d
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-26 20:07:37 +0200

    updated file

--- src/pages/ButtonPage.js
+++ src/pages/ButtonPage.js
@@ -1,21 +1,46 @@
 import Button from "../components/Button";
-import {GoBell, GoCloudDownload, GoDatabase, GoCheck, GoClippy} from 'react-icons/go';
-function ButtonPage (){
-    const handleClick=()=>{
-       
-
-    };
-    return <div>
-        <div><Button success rounded outline className='mb-5' onClick={handleClick}><GoCloudDownload/>Click me </Button></div>
-        <div><Button danger outline> <GoBell/>Buy now!</Button></div>
-        <div><Button warning><GoClippy/>See Data</Button></div>
-        <div><Button primary outline> <GoDatabase/> Hide Ads</Button></div>
-        <div><Button secondary  rounded><GoCheck/>Something!</Button></div>
-       
-
-    </div>;
+import {
+  GoBell,
+  GoCloudDownload,
+  GoDatabase,
+  GoCheck,
+  GoClippy,
+} from "react-icons/go";
+function ButtonPage() {
+  const handleClick = () => {};
+  return (
+    <div>
+      <div>
+        <Button success rounded outline className="mb-5" onClick={handleClick}>
+          <GoCloudDownload />
+          Click me
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          <GoBell />
+          Buy now!
+        </Button>
+      </div>
+      <div>
+        <Button warning>
+          <GoClippy />
+          See Data
+        </Button>
+      </div>
+      <div>
+        <Button primary outline>
+          <GoDatabase /> Hide Ads
+        </Button>
+      </div>
+      <div>
+        <Button secondary rounded>
+          <GoCheck />
+          Something!
+        </Button>
+      </div>
+    </div>
+  );
 }
-
-
 
 export default ButtonPage;
