602c46ec0c6dbf7a4d4a84afecec2d22cd79b8d4
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-26 20:07:29 +0200

    updated file

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -1,19 +1,15 @@
-
 import { useEffect, useState } from "react";
 
-function useCounter (initialCount){
-    const [count, setCount] = useState(initialCount);
+function useCounter(initialCount) {
+  const [count, setCount] = useState(initialCount);
 
-    useEffect(()=>{
-
-    },[count]);
-    const increment = ()=>{
-        setCount(count+1);
-    };
-    return  {
-         count,
-         increment
-    };
-
+  useEffect(() => {}, [count]);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  return {
+    count,
+    increment,
+  };
 }
 export default useCounter;
